adaaa74653101dc0d5057b1773a7954be469a905
岛主_Landor <landor@aliyun.com>
2021-06-03 00:41:27 +0800

    滤波算法

--- User/Filter/Filter.h
+++ User/Filter/Filter.h
@@ -3,6 +3,10 @@
 
 
 int Limit_Filter(int x,int A);
+int Median_Filter(int x,int N);
+int Arithmetic_Average_Filter(int x,int N);
+int Moving_Average_Filter(int x,int N);
+int Median_Average_Filter(int x,int N);
 
 #endif
 
